fef3d4c6002747ae511fcbde02cf73d0a6171f71
Derick Anderson <anderson.derick.w@gmail.com>
2018-09-24 19:57:59 +0000

    deployment

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :memory, MemoryWeb.Endpoint,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn),
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "memory1.ironbeard.com", port: 80],
+  url: [host: "memory1.derickanderson.xyz", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
